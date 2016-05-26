c1e72218ba3d0f7008ed2cfa454df70d8eecac0b
MariaNattestad <lunexa@gmail.com>
2016-05-26 11:37:00 -0700

    Made keep_small_uniques the default in unique anchor filtering

--- Assemblytics_Nchart.R
+++ Assemblytics_Nchart.R
@@ -80,7 +80,7 @@ for (to_png in c(TRUE,FALSE)) {
                 geom_point(data=both.plot,size=2,alpha=0.5) + 
                 labs(x = paste("NG(x)% where 100% = ",bp_format(genome.length), sep=""),y="Sequence length",colour="Assembly",title="Cumulative sequence length") +
                 scale_color_manual(values=colors) +
-                annotation_logticks(sides="lr")
+                annotation_logticks(sides="lr",color="black")
               )
     } else {
         # To make bacterial genomes at least show a dot instead of an error because  
